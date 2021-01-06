a39f5b5524916e6e75659c026f0ae02817903dfe
Macenna Cowen <38767248+Macenna@users.noreply.github.com>
2021-01-06 18:16:05 -0500

    Changing the question function format

--- script.js
+++ script.js
@@ -2,89 +2,111 @@ var quizContainer = document.getElementById("quiz");
 var resultsContainer = document.getElementById("results");
 var submitBtn = document.getElementById("submit");
 
+submitBtn.addEventListener("click", buildCard)
 
 // Questions 
 var questions = [
-        // 1
-    {question: "What does HTML stand for?",
-    answers: {
-        a: "Hyper Technology Masking Link",
-        b: "Hydro Technological Metric Language",
-        c: "Hypertext Markup Language"
+    // 1 - Questions object, w/in it there's a question string and then an answers array, and then a correct answer string
+    // Looping over something is an array method 
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            "Hyper Technology Masking Link",
+            "Hydro Technological Metric Language",
+            "Hypertext Markup Language"
+        ],
+        correctAnswer: "Hypertext Markup Language"
     },
-    correctAnswer: "c"},
-        // 2
-    {question: "What does CSS Stand for?",
-    answers: {
-        a: "Cascading Style Sheets",
-        b: "Conforming Selector Styles",
-        c: "Converging Style Selectors"
+    // 2
+    {
+        question: "What does CSS Stand for?",
+        answers: [
+            "Cascading Style Sheets",
+            "Conforming Selector Styles",
+            "Converging Style Selectors"
+        ],
+        correctAnswer: "Cascading Style Sheets"
     },
-    correctAnswer: "a"},
-        // 3
-    {question: "What does # define in an HTML file?",
-    answers: {
-        a: "ID Name",
-        b: "Class Name",
-        c: "Tag"
+    // 3
+    {
+        question: "What does # define in an HTML file?",
+        answers: [
+            "ID Name",
+            "Class Name",
+            "Tag"
+        ],
+        correctAnswer: "ID Name"
     },
-    correctAnswer: "a"},
-        // 4
-    {question: "What does '.' call in a HTML/CSS/JS file?",
-    answers: {
-        a: "Tag",
-        b: "Class Name",
-        c: "ID Name"
+    // 4
+    {
+        question: "What does '.' call in a HTML/CSS/JS file?",
+        answers: [
+            "Tag",
+            "Class Name",
+            "ID Name"
+        ],
+        correctAnswer: "Class Name"
     },
-    correctAnswer: "b"},
-        // 5
-    {question: "Where do you link a CSS file?",
-    answers: {
-        a: "Body Tag",
-        b: "Footer",
-        c: "Head Tag"
+    // 5
+    {
+        question: "Where do you link a CSS file?",
+        answers: [
+            "Body Tag",
+            "Footer",
+            "Head Tag"
+        ],
+        correctAnswer: "Head Tag"
     },
-    correctAnswer: "c"},
-        // 6
-    {question: "Where do you link a JS file?",
-    answers: {
-        a: "Body Tag",
-        b: "Head Tag",
-        c: "Header"
+    // 6
+    {
+        question: "Where do you link a JS file?",
+        answers: [
+            "Body Tag",
+            "Head Tag",
+            "Header"
+        ],
+        correctAnswer: "Body Tag"
     },
-    correctAnswer: "a"},
-        // 7
-    {question: "Where do you link jQuery CDN?",
-    answers: {
-        a: "Body Tag",
-        b: "Head Tag",
-        c: "Footer"
+    // 7
+    {
+        question: "Where do you link jQuery CDN?",
+        answers: [
+            "Body Tag",
+            "Head Tag",
+            "Footer"
+        ],
+        correctAnswer: "Head Tag"
     },
-    correctAnswer: "b"},
-        // 8
-    {question: "Where do you link external CSS style sheets from Bootstrap?",
-    answers: {
-        a: "Body Tag",
-        b: "Footer",
-        c: "Head Tag"
+    // 8
+    {
+        question: "Where do you link external CSS style sheets from Bootstrap?",
+        answers: [
+            "Body Tag",
+            "Footer",
+            "Head Tag"
+        ],
+        correctAnswer: "Head Tag"
     },
-    correctAnswer: "c"},
-        // 9
-    {question: "What is an API?",
-    answers: {
-        a: "Application Programming Interface",
-        b: "Application Planner Interface",
-        c: "Application Printing Initializer"
+    // 9
+    {
+        question: "What is an API?",
+        answers: [
+            "Application Programming Interface",
+            "Application Planner Interface",
+            "Application Printing Initializer"
+        ],
+        correctAnswer: "Application Programming Interface"
     },
-    correctAnswer: "a"},
-        // 10
-    {question: "Where do you link an API?",
-    answers: {
-        a: "HTML File",
-        b: "Javascript File",
-        c: "CSS File"
+    // 10
+    {
+        question: "Where do you link an API?",
+        answers: [
+            "HTML File",
+            "Javascript File",
+            "CSS File"
+        ],
+        correctAnswer: "Javascript File"
     },
-    correctAnswer: "b"},
 ];
 
 
@@ -92,170 +114,177 @@ var questions = [
 var prevBtn = document.getElementById("previous");
 var nextBtn = document.getElementById("next");
 var slides = document.querySelectorAll(".slide");
-var currentSlide = 0;
+var index = 0;
 
 
 // Function - build quiz itself - run immediately 
-function buildQuiz() {
-    // Variable to store HTML output
-    var output = [];
+function buildCard() {
+    // Append things to the page (DOM interactions)
+    var currentQuestion = questions[index].question;
 
-    // Method to call each question 
-    questions.forEach(                                      // forEach loop returns: current value, the index, & the array itself as parameters 
-        (currentQuestion, questionNumber) => {                     // => Arrow function for operations - we only need current value & index - so name currentQuestion & questionNumber, respectfully 
-            // Local var to store list of possible answers 
-            var answers = [];
-            // for loop for each multiple choice option 
-            for(letter in currentQuestion.answers){
-                
-                // Add an HTML radio button - add "backticks" (` `) around HTML 
-                answers.push(               // Label element allows users to click anywhere on the answer text to select that answer (more accessible) (if this label section wasn't here, user would have to click on the radio button itself)
-                    `<label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}"></input>
-                        ${letter} : 
-                        ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            };
+    var questionTitle = document.getElementById("questionTitle"); 
 
-            // Add this question & its answers to the output (display question for user to answer)
-            output.push(
-                `<div class="slide">
-                    <div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join("")} </div>
-                </div>`
-            );                                                              // join expression takes list of answers & puts them together in one string that we can output into our answers div
-        }
-    );
+    var answers = document.getElementById("answers");
+        // Dumps the previous question's choices for the next question's choices 
+        // Makes children in div go away 
+    answers.innerHTML = "";
 
-    // Combine outpit list into 1 string of HTML & put it on the page
-    quizContainer.innerHTML = output.join("");
+    questionTitle.textContent = currentQuestion;
+
+    questions[index].answers.forEach(function (answer) {
+        var button = document.createElement("button"); 
+            // Creating an element that doesn't yet exist in the DOM
+        button.textContent = answer;
+            // Creates 3 buttons w/ different answers (creates as many options as written in object)
+        button.setAttribute("value", answer);
+
+        button.onclick = evaluateAnswer; 
+
+        answers.appendChild(button);
+    });
+
 };
+
+function evaluateAnswer() {
+    console.log(this.value);
+    //use if statements to evaluate the answers
+    index++;
+    if (index === questions.length) {
+        console.log("game over");
+        //create a function to end the game
+    } else {
+        buildCard();
+    };
+   
+};
+
+
+
 
 
 // Function - Show Quiz results - Run when user clicks submit button
-    // function loops over the answers, checks them, & shows the results 
-function showResults() {
-    // Call answer containers from quiz - create variables to keep track of user's current answer & total number of correct answers 
-    var answerContainers = quizContainer.querySelectorAll(".answers");
+// function loops over the answers, checks them, & shows the results 
+//function showResults() {
+//     // Call answer containers from quiz - create variables to keep track of user's current answer & total number of correct answers 
+//     var answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // Keep track of user's answers
-    var numCorrect = 0;
+//     // Keep track of user's answers
+//     var numCorrect = 0;
 
-    // forEach question - loop through each question & check the answers 
-    questions.forEach( (currentQuestion, questionNumber) => {
-        // Find selected answer
-        var answerContainer = answerContainers[questionNumber];                         // Looking inside answer container for current question
-        var selector = `input[name=question${questionNumber}]:checked`;                 // Defining CSS Selector that will find which radio button is checked 
-        var userAnswer = (answerContainer.querySelector(selector) || {}).value;     // querySelector searches for CSS selector in previously defined answerContainer to find which answer's radio button is checked 
-            // Get reference to selected answer element, if that doesn't exist, use an empty object & get the value of whatever was in the first statement --- Get the value of ^ answer w/ .nodeValue 
-                // As a result, the value will either be the user's answer or undefined, which means a user can skip a question w/o crashing quiz 
+//     // forEach question - loop through each question & check the answers 
+//     questions.forEach((currentQuestion, questionNumber) => {
+//         // Find selected answer
+//         var answerContainer = answerContainers[questionNumber];                         // Looking inside answer container for current question
+//         var selector = `input[name=question${questionNumber}]:checked`;                 // Defining CSS Selector that will find which radio button is checked 
+//         var userAnswer = (answerContainer.querySelector(selector) || {}).value;     // querySelector searches for CSS selector in previously defined answerContainer to find which answer's radio button is checked 
+//         // Get reference to selected answer element, if that doesn't exist, use an empty object & get the value of whatever was in the first statement --- Get the value of ^ answer w/ .nodeValue 
+//         // As a result, the value will either be the user's answer or undefined, which means a user can skip a question w/o crashing quiz 
 
-        // if statement - if correct / else incorrect 
-        if (userAnswer === currentQuestion.correctAnswer) {
-            // Add to the correct counter
-            numCorrect++;
-            // Color the answers green
-            answerContainer[questionNumber].style.color = "green";
-        }
-        // if incorrect or skipped 
-        else {
-            // Color the answers red
-            answerContainer[questionNumber].style.color = "red";
-        }
-    });
-    // Show # of correct answers out of total 
-    resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
-};
+//         // if statement - if correct / else incorrect 
+//         if (userAnswer === currentQuestion.correctAnswer) {
+//             // Add to the correct counter
+//             numCorrect++;
+//             // Color the answers green
+//             answerContainer[questionNumber].style.color = "green";
+//         }
+//         // if incorrect or skipped 
+//         else {
+//             // Color the answers red
+//             answerContainer[questionNumber].style.color = "red";
+//         }
+//     });
+//     // Show # of correct answers out of total 
+//     resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+// };
 
-// Display quiz in real time
-buildQuiz();
+// // Display quiz in real time
+// buildCard();
 
-// var for active slide 
+// // var for active slide 
 
-// Function to show a slide --- Have to add class for active slide 
-function showSlide(n) {
-    slides[currentSlide].classList.remove("active-slide");      // Hides current slide 
-    slides[n].classList.add("active-slide");                    // Shows the new slide 
-    currentSlide = n;                                           // Updates current slide number 
-    if (currentSlide === 0) {                                   // if on the 1st slide, hide the prev slide button, o.w. show the button
-        prevBtn.style.display = "none";
-    }
-    else {
-        prevBtn.style.display = "inline-block";
-    }
-    if (currentSlide === slides.length-1) {                     // if on the last slide, hide the next slide button & show the submit button
-        nextBtn.style.display = "none";
-        submitBtn.style.display = "inline-block";
-    }
-    else {                                                      // o.w. show the next slide button & hide the submit (for now)
-        nextBtn.style.display = "inline-block";
-        submitBtn.style.display = "none";
-    }
-};
+// // Function to show a slide --- Have to add class for active slide 
+// function showSlide(n) {
+//     slides[currentSlide].classList.remove("active-slide");      // Hides current slide 
+//     slides[n].classList.add("active-slide");                    // Shows the new slide 
+//     currentSlide = n;                                           // Updates current slide number 
+//     if (currentSlide === 0) {                                   // if on the 1st slide, hide the prev slide button, o.w. show the button
+//         prevBtn.style.display = "none";
+//     }
+//     else {
+//         prevBtn.style.display = "inline-block";
+//     }
+//     if (currentSlide === slides.length - 1) {                     // if on the last slide, hide the next slide button & show the submit button
+//         nextBtn.style.display = "none";
+//         submitBtn.style.display = "inline-block";
+//     }
+//     else {                                                      // o.w. show the next slide button & hide the submit (for now)
+//         nextBtn.style.display = "inline-block";
+//         submitBtn.style.display = "none";
+//     }
+// };
 
-// Adding functionality to navigation buttons 
-    // Use showSlide function in each of these functions 
-function showNextSlide() {
-    showSlide(currentSlide + 1);
-};
-function showPrevSlide() {
-    showSlide(currentSlide - 1);
-};
+// // Adding functionality to navigation buttons 
+// // Use showSlide function in each of these functions 
+// function showNextSlide() {
+//     showSlide(currentSlide + 1);
+// };
+// function showPrevSlide() {
+//     showSlide(currentSlide - 1);
+// };
 
-// calls funtion to show the first slide 
-showSlide(currentSlide);
-
-
-// Highscores 
-var result = {userName: user, score: timeScore};
-var savedScores = localStorage.getItem("highscore") || "[]";     // Get the score, or the intial value if empty 
-
-var highscores = [JSON.parse(savedScores), result]           // Add the result 
-    .sort((a, b) => b.score - a.score)                          // Score descending 
-    .slice(0, 5);        // Take highest 5 (scores)
-
-localStorage.setItem("highscore", JSON.stringify(highscores));   // Store the scores 
+// // calls funtion to show the first slide 
+// showSlide(currentSlide);
 
 
-// Vars for Timer
-var timeInMinutes = 10;
-var currentTime = Date.parse(new Date());
-var deadline = new Date(currentTime + timeInMinutes*60*1000);
+// // Highscores 
+// var result = { userName: user, score: timeScore };
+// var savedScores = localStorage.getItem("highscore") || "[]";     // Get the score, or the intial value if empty 
 
-// Timer Function
-function getTimeRemaining(endtime) {
-    var total = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((total / 1000) % 60);
+// var highscores = [JSON.parse(savedScores), result]           // Add the result 
+//     .sort((a, b) => b.score - a.score)                          // Score descending 
+//     .slice(0, 5);        // Take highest 5 (scores)
 
-    return {
-        total,
-        seconds
-    };
-};
-
-function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var secondsSpan = clock.querySelector(".seconds");
-
-    function updateClock () {
-        var t = getTimeRemaining(endtime);
-
-        secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
-
-        if (t.total <= 0 ) {
-            clearInterval(timeInterval);
-        }
-    }
-    updateClock();
-    var timeInterval = setInterval(updateClock, 1000);
-};
-
-// Call clock 
-initializeClock("clockdiv", deadline);
+// localStorage.setItem("highscore", JSON.stringify(highscores));   // Store the scores 
 
 
-// On submit, show results to user 
-prevBtn.addEventListener("click", showPrevSlide);
-nextBtn.addEventListener("click", showNextSlide);
-submitBtn.addEventListener("click", showResults);
+// // Vars for Timer
+// var timeInMinutes = 10;
+// var currentTime = Date.parse(new Date());
+// var deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
+
+// // Timer Function
+// function getTimeRemaining(endtime) {
+//     var total = Date.parse(endtime) - Date.parse(new Date());
+//     var seconds = Math.floor((total / 1000) % 60);
+
+//     return {
+//         total,
+//         seconds
+//     };
+// };
+
+// function initializeClock(id, endtime) {
+//     var clock = document.getElementById(id);
+//     var secondsSpan = clock.querySelector(".seconds");
+
+//     function updateClock() {
+//         var t = getTimeRemaining(endtime);
+
+//         secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+//         if (t.total <= 0) {
+//             clearInterval(timeInterval);
+//         }
+//     }
+//     updateClock();
+//     var timeInterval = setInterval(updateClock, 1000);
+// };
+
+// // Call clock 
+// initializeClock("clockdiv", deadline);
+
+
+// // On submit, show results to user 
+// prevBtn.addEventListener("click", showPrevSlide);
+// nextBtn.addEventListener("click", showNextSlide);
+// submitBtn.addEventListener("click", showResults);
